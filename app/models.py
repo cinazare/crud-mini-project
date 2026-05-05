a92e5714dc738a2129e2f1b99dd5fe7afbe955bb
cina.z.e <its.cina.zare@gmail.com>
2026-05-05 21:26:10 +0330

    database model developed not tested

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from typing import Optional
+from sqlmodel import Field, SQLModel
+from datetime import datetime
+
+
+class Task(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title: str
+    description: Optional[str] = None
+    is_completed: bool = Field(default=False)
+    created_at: datetime = Field(default_factory=datetime.now())
